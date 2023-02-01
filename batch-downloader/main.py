5f7c3dfdd64cd6850bba849eadbe60aa97743974
Potchama <114906811+rynnzhan@users.noreply.github.com>
2023-02-02 00:48:12 +0800

    Update main.py

--- batch-downloader/main.py
+++ batch-downloader/main.py
@@ -1,7 +1,7 @@
 import wget
 import os
 
-folder_path = 'C:\\Users\\Ryou\\Pictures\\neru_nagahama\\hitoiro'
+folder_path = 'C:\\Users\\Pictures\\img'
 if not os.path.exists(folder_path):
     os.mkdir(folder_path)
 
@@ -9,12 +9,12 @@ number = 50
 
 for i in range(1, number+1):
     if i < 10:
-        index = '0'+str(i)
+        index = '0'+str(i) #个位数前加'0'
     else:
         index = str(i)
 
     path = f'{folder}\\neru_img{index}.jpg'
-    url = f'https://hito-iro.jp/wp/wp-content/uploads/2023/01/neru-nagahama_{index}.jpg'
+    url = f'https://xxxx.com/xxxxx/xxxx/ooxx_img_{index}.jpg'
     print(f"\nDownlaoing img{index}")
     wget.download(url, path)
 
